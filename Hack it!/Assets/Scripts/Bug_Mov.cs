a17a70df9217f9b1e4ad1083dea0f9f87520e36c
Emilio Ingenito <emilio.ingenito@icloud.com>
2020-11-09 18:12:28 +0100

    Bug movement

---
+++ Hack it!/Assets/Scripts/Bug_Mov.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bug_Mov : MonoBehaviour
+{
+    public GameObject bug;
+	public Transform pos1, pos2;
+	public float speed;
+	public Transform startPos;
+
+	Vector3 nextPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPos = startPos.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position == pos1.position)
+        {
+        	nextPos = pos2.position;
+            bug.transform.localScale = new Vector3(6, 6, 0);
+        }
+
+        if(transform.position == pos2.position)
+        {
+        	nextPos = pos1.position;
+            bug.transform.localScale = new Vector3(-6, 6, 0);
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed*Time.deltaTime);
+
+    }
+
+
+
+}
