542aebe372904b13302e3170131cc95e38935829
Emma <emma.gagliani.caputo@gmail.com>
2020-11-09 17:39:59 +0100

    joistick prova

---
+++ Hack it!/Assets/Scripts/Comportamento_Collisioni.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Comportamento_Collisioni : MonoBehaviour
+{
+   
+
+    public IEnumerator OnCollisionEnter2D (Collision2D collision)
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+       // Animator animator = player.GetComponent<Animator>();
+
+        if (collision.gameObject.Equals(player))
+        {
+            //animator.Play("Death");
+            yield return new WaitForSeconds(1);
+            SceneManager.LoadScene("Livello_01");
+
+        }
+
+    }
+}
---
+++ Hack it!/Assets/Scripts/Comportamento_Collisioni_Bug.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comportamento_Collisioni_Bug : MonoBehaviour
+{
+   
+    public GameObject palyer;
+       
+
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
