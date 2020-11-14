8ee4f0b13c9495c55608a2de557e1cfc5d421d07
Emma <emma.gagliani.caputo@gmail.com>
2020-11-14 18:32:52 +0100

    aggiunta musica statica e collisione con backdoor

---
+++ Hack it!/Assets/Scripts/Fine_Livello.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fine_Livello : MonoBehaviour
+{
+    GameObject player;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (collision.gameObject.Equals(player))
+        {
+            //qui verrà messo il controllo sul numero minimo di oggetti raccolti
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
