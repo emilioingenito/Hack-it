6f1a07389f4fd586d973cad699cfdc1924007bc5
Emma <emma.gagliani.caputo@gmail.com>
2020-11-17 12:15:48 +0100

    sistemato movimento

--- Hack it!/Assets/Scripts/Bug_Mov.cs
+++ Hack it!/Assets/Scripts/Bug_Mov.cs
@@ -21,16 +21,16 @@ public class Bug_Mov : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if(transform.position.x == pos1.position.x)
+        if(transform.position == pos1.position)
         {
         	nextPos = pos2.position;
             spriteRenderer.flipX = true;
             // bug.transform.localScale = new Vector3(6, 6, 0);
         }
 
-        if(transform.position.x == pos2.position.x)
+        if(transform.position == pos2.position)
         {
         	nextPos = pos1.position;
             spriteRenderer.flipX = false;
