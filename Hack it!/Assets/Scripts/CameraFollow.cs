43dbb71b5e83af3676820df107521e6e6c91aafb
Emilio Ingenito <emilio.ingenito@icloud.com>
2021-01-03 17:16:16 +0100

    Parallax

---
+++ Hack it!/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+    [Range(0.01f, 1.0f)]
+    public float smoothSpeed = 0.5f;
+
+    void Start()
+    {
+    	offset = transform.position - target.position;
+    }
+
+    void LateUpdate() 
+    {
+    	Vector3 newPos = target.position + offset;
+    	transform.position = Vector3.Slerp(transform.position, newPos, smoothSpeed);
+    }
+}
